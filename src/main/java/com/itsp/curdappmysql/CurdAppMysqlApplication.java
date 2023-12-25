93e4beb562dff580e3c3afb8bda2b7130d7a560f
itrikcode <kumarviveklohaman@gmail.com>
2023-12-25 22:08:37 +0530

    remove sop

--- src/main/java/com/itsp/curdappmysql/CurdAppMysqlApplication.java
+++ src/main/java/com/itsp/curdappmysql/CurdAppMysqlApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CurdAppMysqlApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test");
-		System.out.println("more testing");
 		SpringApplication.run(CurdAppMysqlApplication.class, args);
 	}
 
