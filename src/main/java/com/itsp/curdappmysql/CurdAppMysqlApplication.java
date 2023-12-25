93d7bd0c695685787af7b619b5338e93f6c90dc2
itrikcode <kumarviveklohaman@gmail.com>
2023-12-25 21:57:13 +0530

    first commit

---
+++ src/main/java/com/itsp/curdappmysql/CurdAppMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.itsp.curdappmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdAppMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdAppMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsp/curdappmysql/CurdAppMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsp.curdappmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdAppMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
